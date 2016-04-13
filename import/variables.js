e02e955314f0f20f598d138c298ae748a851831e
Pavel Šeps <pavel.seps@layoutmaschine.de>
2016-04-13 16:04:17 +0200

    add default variables

---
+++ import/variables.js
@@ -0,0 +1,34 @@
+//program for work
+var programsForWork = [
+    ["Test program", "C:\\myprogram.exe", false]
+];
+
+/**
+ * root folder for projects
+ * folder
+ */
+const rootProjectDir = "";
+
+/**
+ * fcZpravy()
+ * url
+ */
+const zpravy = '';
+
+/**
+ * fcJira()
+ * url
+ */
+const jira = '';
+
+/**
+ * fcZabava()
+ * url
+ */
+const zabava = '';
+
+/**
+ * fcPoznamky()
+ * file
+ */
+const poznamky = "";
